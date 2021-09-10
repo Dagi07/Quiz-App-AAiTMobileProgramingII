d1874df2f7f01478fb9a2e83ed2e6b1414f42579
SurafelWT <56756903+SurafelWT@users.noreply.github.com>
2021-09-10 18:49:11 +0300

    Add files via upload

---
+++ lib/data/leaderboard/screens/leaderboard.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class LeaderboardScreen extends StatelessWidget {
+  static const routeName = '/leaderboard';
+  const LeaderboardScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text("hi"),
+    );
+  }
+}
