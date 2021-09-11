0fd4f78e8f76b2310ad6eb364735865193b4a088
Dagi07 <56720713+Dagi07@users.noreply.github.com>
2021-09-11 16:21:14 +0300

    Add files via upload

---
+++ quiz_proj/lib/data/category/model/category.dart
@@ -0,0 +1,20 @@
+class Category {
+  final String id;
+  final String name;
+
+  const Category({this.id = "", required this.name});
+
+  factory Category.fromJson(Map<String, dynamic> json) {
+    return Category(
+      id: json['_id'],
+      name: json['name'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['_id'] = this.id;
+    data['name'] = this.name;
+    return data;
+  }
+}
